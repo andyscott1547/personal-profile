20f9b3fa1a0e28ac7e83268a6ce9ad708027b025
Andy Scott <andy.scott@bjss.com>
2023-04-06 16:12:09 +0100

    add backend and s3 access logging

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 # main
 
 module "cdn" {
-  source      = "./modules/s3_cloudfront"
+  source = "./modules/s3_cloudfront"
   providers = {
     aws.global = aws.global
   }
 
-  name        = "pegasustechnology"
-  domain_name = "pegasus-technology.co.uk"
+  name                         = "pegasustechnology"
+  domain_name                  = "pegasus-technology.co.uk"
+  access_logging_target_bucket = "502101718834-eu-west-1-access-logs"
 }
 
 resource "aws_s3_object" "this" {
-  for_each = fileset("./src", "*")
-  bucket   = module.cdn.bucket_name
-  key      = each.value
-  source   = "./src/${each.value}"
-  etag     = filemd5("./src/${each.value}")
+  for_each     = fileset("./src", "*")
+  bucket       = module.cdn.bucket_name
+  key          = each.value
+  source       = "./src/${each.value}"
+  etag         = filemd5("./src/${each.value}")
   content_type = "text/html"
 }
--- modules/s3_cloudfront/providers.tf
+++ modules/s3_cloudfront/providers.tf
@@ -4,9 +4,9 @@ terraform {
   required_version = "~> 1.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-      configuration_aliases = [ aws.global]
+      source                = "hashicorp/aws"
+      version               = "~> 4.0"
+      configuration_aliases = [aws.global]
     }
   }
 }
